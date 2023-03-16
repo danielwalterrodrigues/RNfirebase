4b33ae7f32dc0d61e17594c9bd5a5f39967babc5
danielwalterrodrigues <danielwalterrodrigues@gmail.com>
2023-03-16 00:17:06 -0300

    commit inicla

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const defaultAvatar = 'https://firebasestorage.googleapis.com/v0/b/expo-boilerplate-v2.appspot.com/o/icon.png?alt=media&token=505f30e6-6702-410a-84e7-f66b3ff60daf'
+
+const firebaseKey = {
+  apiKey: "AIzaSyA-OHK-MIZ_v3-uRoGfl8fj208zdigqpBI",
+  authDomain: "proximadx.firebaseapp.com",
+  projectId: "proximadx",
+  storageBucket: "proximadx.appspot.com",
+  messagingSenderId: "911203737556",
+  appId: "1:911203737556:web:ebd285e647751e1dea8041",
+  measurementId: "G-D9QR8B7NBR"
+}
+
+const eulaLink = 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+
+const expoProjectId = '1650611b-a5b8-4420-9656-60c06429edd4'
+
+export { defaultAvatar, firebaseKey, eulaLink, expoProjectId }
